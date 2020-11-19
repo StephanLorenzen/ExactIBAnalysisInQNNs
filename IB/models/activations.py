4fdfe2ee13a7ab60c0357f09434981577adb94ec
Stephan Lorenzen <lorenzen@di.ku.dk>
2020-11-19 22:22:52 +0100

    Fixed small bug in activations.py

--- IB/models/activations.py
+++ IB/models/activations.py
@@ -1,8 +1,9 @@
 def get_activation_bound(act_func):
+    act_func = act_func.lower()
     act_funcs = {
         "tanh":(-1.0,1.0),
         "relu":(0.0,None),
     }
     if act_func not in act_funcs:
         raise Exception("Unknown activation function: '"+str(act_func)+"'")
-    return act_funcs[act_func.lower()]
+    return act_funcs[act_func]

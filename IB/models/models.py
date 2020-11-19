06d0d53bae55b013362949d2b0a820fd9ac391a2
Stephan Lorenzen <lorenzen@di.ku.dk>
2020-11-19 22:51:04 +0100

    Fixed up models, left old version in comment.

--- IB/models/models.py
+++ IB/models/models.py
@@ -1,15 +1,36 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras 
+from tensorflow.keras import layers as L
 
-def shwartz_ziv_99(activation='tanh'):
-    layers = [10,7,5,4,3] # input dim=12, output neurons (2) added in the end
-    model = keras.Sequential()
-    for i,lsize in enumerate(layers):
-        l_init = keras.initializers.TruncatedNormal(mean=0., stddev=1./float(np.sqrt(lsize)))
-        if i==0:
-            model.add(keras.layers.Dense(lsize, input_dim=12, activation=activation, kernel_initializer=l_init))
-        else:
-            model.add(keras.layers.Dense(lsize, activation=activation, kernel_initializer=l_init))
-    model.add(keras.layers.Dense(2, activation='softmax'))
-    return model
+def FNN(layers, input_dim=12, activation='tanh', init=None):
+    if init not in [None, 'truncated_normal']:
+        raise Exception('Unknown initializer...')
+    
+    if init=='truncated_normal':
+        def k_init(l):
+            return keras.initializers.TruncatedNormal(mean=0., stddev=1./float(np.sqrt(l)))
+    else:
+        def k_init(l):
+            return None
+
+    k_layers = [keras.layers.InputLayer(input_shape=input_dim)]
+    for l in layers:
+        k_layers.append(L.Dense(l, activation=activation, kernel_initializer=k_init(l)))
+    k_layers.append(L.Dense(2, activation='softmax'))
+
+    return keras.Sequential(k_layers)
+
+def shwartz_ziv_99(activation='tanh', init='truncated_normal'):
+    return FNN([10,7,5,4,3], input_dim=12, activation='tanh', init=init)
+
+def _shwartz_ziv_99(activation='tanh'):
+    return keras.Sequential([
+        keras.layers.InputLayer(input_shape=12),
+        keras.layers.Dense(10, activation=activation),
+        keras.layers.Dense(7, activation=activation),
+        keras.layers.Dense(5, activation=activation),
+        keras.layers.Dense(4, activation=activation),
+        keras.layers.Dense(3, activation=activation),
+        keras.layers.Dense(2, activation="softmax")
+    ])

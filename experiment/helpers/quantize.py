0b2332e83fc1e2eb7e899907d5c5f7143653d580
Stephan Lorenzen <lorenzen@di.ku.dk>
2021-08-24 10:04:43 +0200

    Updated quantization experiment (still needs tweaking).

--- experiment/helpers/quantize.py
+++ experiment/helpers/quantize.py
@@ -1,26 +1,39 @@
 import sys
 
 from IB.models import load as load_model
-from IB.util.estimator import QuantizedEstimator
+from IB.util.estimator import BinningEstimator, QuantizedEstimator
 from IB.experiment import run_experiment
 
 if len(sys.argv)<3:
-    print("Missing arguments, quantize.py <act_function> <bits>.")
+    print("Missing arguments, binning.py <experiment> <bits> [repeats=50].")
     sys.exit(1)
 
 # Activation function
-act_fun = sys.argv[1]
+exp     = sys.argv[1]
 bits    = int(sys.argv[2])
-repeats = 50 
+repeats = int(sys.argv[3]) if len(sys.argv)>=4 else 50
+
+if exp not in ("MNIST","CIFAR","SYN-Tanh","SYN-ReLU"):
+    print("Experiment must be one of 'MNIST', 'CIFAR' or 'SYN-{Tanh,ReLU}'")
+    sys.exit(1)
 
 # Model
-_model = load_model("ShwartzZiv99")
-Model = lambda: (_model(activation=act_fun, quantize=(bits<=16), num_bits=bits), bits<=16)
+if exp[:3]=="SYN":
+    _model  = load_model("SYN")
+    act_fun = exp[4:].lower()
+    dname   = "SYN"
+    Model   = lambda: (_model(activation=act_fun, quantize=(bits<=16), num_bits=bits), (bits<=16))
+    epochs  = 8000
+else:
+    _model = load_model(exp)
+    dname  = exp
+    Model  = lambda: (_model(quantize=(bits<=16), num_bits=bits), (bits<=16))
+    epochs = 2000
 
 # MI estimators
 estimators = [QuantizedEstimator(bounds="layer", bits=bits)]
 
-res_path = "out/quantized_"+str(bits)+"bit/"+act_fun+"/"
+res_path = "out/quantized/"+exp+"/"+str(bits)+"/"
 
-print("Starting binning experiment.")
-run_experiment(Model, estimators, repeats=repeats, out_path=res_path)
+print("Starting quantized experiment.")
+run_experiment(Model, estimators, dname, epochs=epochs, repeats=repeats, low_memory=True, out_path=res_path)

45c1c64210a940dacdb0934647cab0baaf6b7cb7
Stephan Lorenzen <lorenzen@di.ku.dk>
2021-09-20 09:36:54 +0200

    Corrected multithread bug in experiments

--- experiment/helpers/binning.py
+++ experiment/helpers/binning.py
@@ -4,43 +4,44 @@ from IB.models import load as load_model
 from IB.util.estimator import BinningEstimator, QuantizedEstimator
 from IB.experiment import run_experiment
 
-if len(sys.argv)<2:
-    print("Missing arguments, binning.py <experiment> [repeats=50].")
-    sys.exit(1)
+if __name__ == '__main__':
+    if len(sys.argv)<2:
+        print("Missing arguments, binning.py <experiment> [repeats=50].")
+        sys.exit(1)
 
-# Activation function
-exp     = sys.argv[1]
-repeats = int(sys.argv[2]) if len(sys.argv)>=3 else 50
+    # Activation function
+    exp     = sys.argv[1]
+    repeats = int(sys.argv[2]) if len(sys.argv)>=3 else 50
 
-if exp not in ("MNIST","MNIST-Tanh","MNIST-ReLU","CIFAR","SYN-Tanh","SYN-ReLU"):
-    print("Experiment must be one of 'MNIST[-{Tanh,ReLU}]', 'CIFAR' or 'SYN-{Tanh,ReLU}'")
-    sys.exit(1)
+    if exp not in ("MNIST","MNIST-Tanh","MNIST-ReLU","CIFAR","SYN-Tanh","SYN-ReLU"):
+        print("Experiment must be one of 'MNIST[-{Tanh,ReLU}]', 'CIFAR' or 'SYN-{Tanh,ReLU}'")
+        sys.exit(1)
 
-# Model
-if exp[:3]=="SYN":
-    _model  = load_model("SYN")
-    act_fun = exp[4:].lower()
-    dname   = "SYN"
-    Model   = lambda: (_model(activation=act_fun, quantize=False), False)
-    epochs  = 8000
-elif exp=="MNIST-Tanh" or exp=="MNIST-ReLU":
-    act_fun = exp[6:].lower()
-    _model  = load_model("MNIST-FC")
-    dname   = "MNIST"
-    Model   = lambda: (_model(activation=act_fun,quantize=False), False)
-    epochs  = 2000
-else:
-    _model = load_model(exp)
-    dname  = exp
-    Model  = lambda: (_model(quantize=False), False)
-    epochs = 2000
+    # Model
+    if exp[:3]=="SYN":
+        _model  = load_model("SYN")
+        act_fun = exp[4:].lower()
+        dname   = "SYN"
+        Model   = lambda: (_model(activation=act_fun, quantize=False), False)
+        epochs  = 8000
+    elif exp=="MNIST-Tanh" or exp=="MNIST-ReLU":
+        act_fun = exp[6:].lower()
+        _model  = load_model("MNIST-FC")
+        dname   = "MNIST"
+        Model   = lambda: (_model(activation=act_fun,quantize=False), False)
+        epochs  = 2000
+    else:
+        _model = load_model(exp)
+        dname  = exp
+        Model  = lambda: (_model(quantize=False), False)
+        epochs = 2000
 
-# MI estimators
-estimators = []
-for n_bins in [30,100,256]:
-    estimators.append(BinningEstimator("uniform", n_bins=n_bins, bounds="global"))
+    # MI estimators
+    estimators = []
+    for n_bins in [30,100,256]:
+        estimators.append(BinningEstimator("uniform", n_bins=n_bins, bounds="global"))
 
-res_path = "out/binning/"+exp+"/"
+    res_path = "out/binning/"+exp+"/"
 
-print("Starting binning experiment.")
-run_experiment(Model, estimators, dname, epochs=epochs, repeats=repeats, out_path=res_path)
+    print("Starting binning experiment.")
+    run_experiment(Model, estimators, dname, epochs=epochs, repeats=repeats, out_path=res_path)
--- experiment/helpers/quantize.py
+++ experiment/helpers/quantize.py
@@ -4,42 +4,43 @@ from IB.models import load as load_model
 from IB.util.estimator import BinningEstimator, QuantizedEstimator
 from IB.experiment import run_experiment
 
-if len(sys.argv)<3:
-    print("Missing arguments, binning.py <experiment> <bits> [repeats=50].")
-    sys.exit(1)
+if __name__ == '__main__':
+    if len(sys.argv)<3:
+        print("Missing arguments, binning.py <experiment> <bits> [repeats=50].")
+        sys.exit(1)
 
-# Activation function
-exp     = sys.argv[1]
-bits    = int(sys.argv[2])
-repeats = int(sys.argv[3]) if len(sys.argv)>=4 else 50
+    # Activation function
+    exp     = sys.argv[1]
+    bits    = int(sys.argv[2])
+    repeats = int(sys.argv[3]) if len(sys.argv)>=4 else 50
 
-if exp not in ("MNIST","MNIST-Tanh","MNIST-ReLU","CIFAR","SYN-Tanh","SYN-ReLU"):
-    print("Experiment must be one of 'MNIST[-{Tanh,ReLU}]', 'CIFAR' or 'SYN-{Tanh,ReLU}'")
-    sys.exit(1)
+    if exp not in ("MNIST","MNIST-Tanh","MNIST-ReLU","CIFAR","SYN-Tanh","SYN-ReLU"):
+        print("Experiment must be one of 'MNIST[-{Tanh,ReLU}]', 'CIFAR' or 'SYN-{Tanh,ReLU}'")
+        sys.exit(1)
 
-# Model
-if exp[:3]=="SYN":
-    _model  = load_model("SYN")
-    act_fun = exp[4:].lower()
-    dname   = "SYN"
-    Model   = lambda: (_model(activation=act_fun, quantize=(bits<=16), num_bits=bits), (bits<=16))
-    epochs  = 8000
-elif exp=="MNIST-Tanh" or exp=="MNIST-ReLU":
-    act_fun = exp[6:].lower()
-    _model  = load_model("MNIST-FC")
-    dname   = "MNIST"
-    Model   = lambda: (_model(activation=act_fun,quantize=(bits<=16), num_bits=bits), (bits<=16))
-    epochs  = 2000
-else:
-    _model = load_model(exp)
-    dname  = exp
-    Model  = lambda: (_model(quantize=(bits<=16), num_bits=bits), (bits<=16))
-    epochs = 20
+    # Model
+    if exp[:3]=="SYN":
+        _model  = load_model("SYN")
+        act_fun = exp[4:].lower()
+        dname   = "SYN"
+        Model   = lambda: (_model(activation=act_fun, quantize=(bits<=16), num_bits=bits), (bits<=16))
+        epochs  = 8000
+    elif exp=="MNIST-Tanh" or exp=="MNIST-ReLU":
+        act_fun = exp[6:].lower()
+        _model  = load_model("MNIST-FC")
+        dname   = "MNIST"
+        Model   = lambda: (_model(activation=act_fun,quantize=(bits<=16), num_bits=bits), (bits<=16))
+        epochs  = 2000
+    else:
+        _model = load_model(exp)
+        dname  = exp
+        Model  = lambda: (_model(quantize=(bits<=16), num_bits=bits), (bits<=16))
+        epochs = 20
 
-# MI estimators
-estimators = [QuantizedEstimator(bounds="layer", bits=bits)]
+    # MI estimators
+    estimators = [QuantizedEstimator(bounds="layer", bits=bits)]
 
-res_path = "out/quantized/"+exp+"/"+str(bits)+"/"
+    res_path = "out/quantized/"+exp+"/"+str(bits)+"/"
 
-print("Starting quantized experiment.")
-run_experiment(Model, estimators, dname, epochs=epochs, repeats=repeats, low_memory=True, out_path=res_path)
+    print("Starting quantized experiment.")
+    run_experiment(Model, estimators, dname, epochs=epochs, repeats=repeats, low_memory=True, out_path=res_path)

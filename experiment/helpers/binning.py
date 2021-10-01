dd48196e2ca45bffb693d756d223e068a2f1254a
Stephan Lorenzen <lorenzen@di.ku.dk>
2021-10-01 23:15:08 +0200

    Updated experiments

--- experiment/helpers/binning.py
+++ experiment/helpers/binning.py
@@ -6,18 +6,15 @@ from IB.experiment import run_experiment
 
 if __name__ == '__main__':
     if len(sys.argv)<2:
-        print("Missing arguments, binning.py <experiment> [use_mean=0] [repeats=50].")
+        print("Missing arguments, binning.py <experiment> [repeats=50].")
         sys.exit(1)
 
     # Activation function
     exp      = sys.argv[1]
-    use_mean = (sys.argv[2]=="1") if len(sys.argv)>=3 else False
-    repeats  = int(sys.argv[3]) if len(sys.argv)>=4 else 50
+    repeats  = int(sys.argv[2]) if len(sys.argv)>=3 else 50
 
-    print("Using mean: "+str(use_mean))
-    
-    if exp not in ("MNIST","MNIST-Tanh","MNIST-ReLU","CIFAR","SYN-Tanh","SYN-ReLU"):
-        print("Experiment must be one of 'MNIST[-{Tanh,ReLU}]', 'CIFAR' or 'SYN-{Tanh,ReLU}'")
+    if exp not in ("MNIST-Tanh","MNIST-ReLU","MNIST-Bottleneck","SYN-Tanh","SYN-ReLU"):
+        print("Experiment must be one of 'MNIST[-{Tanh,ReLU,Bottleneck}]' or 'SYN-{Tanh,ReLU}'")
         sys.exit(1)
 
     # Model
@@ -29,22 +26,23 @@ if __name__ == '__main__':
         epochs  = 8000
     elif exp=="MNIST-Tanh" or exp=="MNIST-ReLU":
         act_fun = exp[6:].lower()
-        _model  = load_model("MNIST-FC")
+        _model  = load_model("MNIST-10")
         dname   = "MNIST"
         Model   = lambda: (_model(activation=act_fun,quantize=False), False)
         epochs  = 3000
-    else:
-        _model = load_model(exp)
-        dname  = exp
-        Model  = lambda: (_model(quantize=False), False)
-        epochs = 2000
+    elif exp=="MNIST-Bottleneck":
+        act_fun = "relu" 
+        _model  = load_model("MNIST-Bottleneck")
+        dname   = "MNIST"
+        Model   = lambda: (_model(activation=act_fun,quantize=False), False)
+        epochs  = 3000
 
     # MI estimators
     estimators = []
     for n_bins in [30,100,256]:
-        estimators.append(BinningEstimator("uniform", n_bins=n_bins, bounds="global", use_mean=use_mean))
+        estimators.append(BinningEstimator("uniform", n_bins=n_bins, bounds="global"))
 
-    res_path = "out/binning/"+exp+("-mean" if use_mean else "")+"/"
+    res_path = "out/binning/"+exp+"/"
 
     print("Starting binning experiment.")
     run_experiment(Model, estimators, dname, epochs=epochs, repeats=repeats, out_path=res_path)
--- experiment/helpers/quantize.py
+++ experiment/helpers/quantize.py
@@ -6,19 +6,16 @@ from IB.experiment import run_experiment
 
 if __name__ == '__main__':
     if len(sys.argv)<3:
-        print("Missing arguments, binning.py <experiment> <bits> [use_mean=0] [repeats=50].")
+        print("Missing arguments, binning.py <experiment> <bits> [repeats=50].")
         sys.exit(1)
 
     # Activation function
     exp      = sys.argv[1]
     bits     = int(sys.argv[2])
-    use_mean = (sys.argv[3]=="1") if len(sys.argv)>=4 else False
-    repeats  = int(sys.argv[4]) if len(sys.argv)>=5 else 50
+    repeats  = int(sys.argv[3]) if len(sys.argv)>=4 else 50
 
-    print("Using mean: "+str(use_mean))
-    
-    if exp not in ("MNIST","MNIST-Tanh","MNIST-ReLU","CIFAR","SYN-Tanh","SYN-ReLU"):
-        print("Experiment must be one of 'MNIST[-{Tanh,ReLU}]', 'CIFAR' or 'SYN-{Tanh,ReLU}'")
+    if exp not in ("MNIST-Tanh","MNIST-ReLU","MNIST-Bottleneck","SYN-Tanh","SYN-ReLU"):
+        print("Experiment must be one of 'MNIST[-{Tanh,ReLU,Bottleneck}]' or 'SYN-{Tanh,ReLU}'")
         sys.exit(1)
 
     # Model
@@ -30,20 +27,21 @@ if __name__ == '__main__':
         epochs  = 8000
     elif exp=="MNIST-Tanh" or exp=="MNIST-ReLU":
         act_fun = exp[6:].lower()
-        _model  = load_model("MNIST-FC")
+        _model  = load_model("MNIST-10")
         dname   = "MNIST"
         Model   = lambda: (_model(activation=act_fun,quantize=(bits<=16), num_bits=bits), (bits<=16))
         epochs  = 3000
-    else:
-        _model = load_model(exp)
-        dname  = exp
-        Model  = lambda: (_model(quantize=(bits<=16), num_bits=bits), (bits<=16))
-        epochs = 20
+    elif exp=="MNIST-Bottleneck":
+        act_fun = "relu"
+        _model  = load_model("MNIST-Bottleneck")
+        dname   = "MNIST"
+        Model   = lambda: (_model(activation=act_fun,quantize=(bits<=16), num_bits=bits), (bits<=16))
+        epochs  = 3000
 
     # MI estimators
-    estimators = [QuantizedEstimator(bounds="layer", bits=bits, use_mean=use_mean)]
+    estimators = [QuantizedEstimator(bounds="layer", bits=bits)]
 
-    res_path = "out/quantized/"+exp+("-mean" if use_mean else "")+"/"+str(bits)+"/"
+    res_path = "out/quantized/"+exp+"/"+str(bits)+"/"
 
     print("Starting quantized experiment.")
     run_experiment(Model, estimators, dname, epochs=epochs, repeats=repeats, low_memory=True, out_path=res_path)

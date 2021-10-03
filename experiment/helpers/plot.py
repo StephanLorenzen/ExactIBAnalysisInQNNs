13c7acb9afa79741f01288ce5b3555b4ddf4bfe9
Stephan Lorenzen <lorenzen@di.ku.dk>
2021-10-03 02:08:54 +0200

    Fixed latex plotting in experiment

--- experiment/helpers/plot.py
+++ experiment/helpers/plot.py
@@ -12,17 +12,20 @@ EXP = sys.argv[1]
 
 OUT_DIR = "helpers/latex/"+EXP+"/"
 
-def latex_MI(est,out_dir,data_path):
-    out_dir = OUT_DIR+out_dir
+def latex_MI(est,out_suf,data_path):
+    out_dir = OUT_DIR+out_suf
     if not os.path.isdir(out_dir):
         os.makedirs(out_dir)
     out_file = out_dir+est+".csv"
     print("Creating MI plane file: '"+out_file+"'")
     mean, std = iio.load_MI(data_path,est)
     df  = {"x":[],"y":[],"c":[]}
-    df_layer = dict([("X"+str(i),[]) for i in range(1,6+1)]+[("Y"+str(i),[]) for i in range(1,6+1)])
-    # 8000 /  5 * 6 = 9600
-    mod = 5 if len(mean)>2000 else 1
+    if len(mean) == 5000:
+        mean = mean[:3000]
+        std = std[:3000]
+    # 8000 / 5 * 6 = 9600
+    # 3000 / 2 * 5 = 7500
+    mod = 5 if len(mean)>6000 else 1
     for epoch,mi in enumerate(mean):
         if epoch%mod != 0:
             continue
@@ -32,76 +35,66 @@ def latex_MI(est,out_dir,data_path):
     
     pd.DataFrame(df).to_csv(out_file,index_label="epoch")
 
-def latex_accuracy(acts,data_path,suffix=''):
-    out_dir = OUT_DIR
+def latex_accuracy(out_suf,data_path):
+    out_dir = OUT_DIR+out_suf
     if not os.path.isdir(out_dir):
         os.makedirs(out_dir)
-    out_file = out_dir+"accuracy"+suffix+".csv"
+    out_file = out_dir+"accuracy.csv"
     print("Creating '"+out_file+"'")
     df = dict()
-    for act in acts:
-        train, test = iio.load_accuracy(data_path)
-        df[act+"_train_mean"] = train[0]
-        df[act+"_train_std"] = train[1]
-        df[act+"_train_up"] = train[0]+train[2]
-        df[act+"_train_lw"] = train[0]-train[2]
-        df[act+"_test_mean"] = test[0]
-        df[act+"_test_std"] = test[1]
-        df[act+"_test_up"] = test[0]+test[2]
-        df[act+"_test_lw"] = test[0]-test[2]
+    train, test = iio.load_accuracy(data_path)
+    df["train_mean"] = train[0]
+    df["train_std"] = train[1]
+    df["train_up"] = train[0]+train[2]
+    df["train_lw"] = train[0]-train[2]
+    df["test_mean"] = test[0]
+    df["test_std"] = test[1]
+    df["test_up"] = test[0]+test[2]
+    df["test_lw"] = test[0]-test[2]
     df = pd.DataFrame(df)
-    df = df.iloc[::40]
+    reduce = len(df)//200
+    df = df.iloc[::reduce]
     df.to_csv(out_file,index_label="epoch")
 
+def latex_2D(epochs,data_path,subdir):
+    out_dir = OUT_DIR+subdir
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+    dfs = dict(iio.load_activations_2D(data_path))
+    for epoch in epochs:
+        assert str(epoch) in dfs or str(epoch-1) in dfs, "Missing: "+str(epoch)+" in "+data_path
+        df = dfs.get(str(epoch),dfs[str(epoch-1)])
+        df.to_csv(out_dir+str(epoch)+".csv", index_label="i")
 
 
 if EXP=="binning":
     for nb in [30,100,256]:
-        latex_MI("relu","binning_uniform_"+str(nb)+"_global", "out/binning/")
-        latex_MI("tanh","binning_uniform_"+str(nb)+"_global", "out/binning/")
+        latex_MI("binning_uniform_"+str(nb)+"_global", "SYN-Tanh/", "out/binning/SYN-Tanh/")
+        latex_MI("binning_uniform_"+str(nb)+"_global", "SYN-ReLU/", "out/binning/SYN-ReLU/")
 
 elif EXP=="quantize":
-    latex_MI("quantized_layer_8_bits", "SYN-Tanh/8/", "out/quantized/SYN-Tanh/8/")
-    latex_MI("quantized_layer_8_bits", "SYN-ReLU/8/", "out/quantized/SYN-ReLU/8/")
-    latex_MI("quantized_layer_8_bits", "MNIST/8/",    "out/quantized/MNIST/8/")
+    # Plots for 8-bit quantized synthetic experiments: Tanh and ReLU, I-plane, accuracy
+    latex_MI("quantized_layer_8_bits", "SYN-Tanh/", "out/quantized/SYN-Tanh/8/")
+    latex_MI("quantized_layer_8_bits", "SYN-ReLU/", "out/quantized/SYN-ReLU/8/")
+    latex_accuracy("SYN-Tanh/", "out/binning/SYN-Tanh/")
+    latex_accuracy("SYN-ReLU/", "out/binning/SYN-ReLU/")
+    latex_accuracy("SYN-Tanh/", "out/quantized/SYN-Tanh/8/")
+    latex_accuracy("SYN-ReLU/", "out/quantized/SYN-ReLU/8/")
 
-elif EXP=="accuracy":
-    latex_accuracy(["relu","tanh"], "out/binning/", suffix="_binning")
-    latex_accuracy(["relu","tanh"], "out/quantized_8bit/", suffix="_8bit")
+elif EXP=="MNIST-Bottleneck":
+    # Plots for MNIST bottleneck: 2D plot, I-plane, accuracy
+    latex_2D([200,1000,3000], "out/quantized/MNIST-Bottleneck-old/8/", "2D/")
+    latex_MI("quantized_layer_8_bits", "", "out/quantized/MNIST-Bottleneck-old/8/")
+    latex_accuracy("", "out/quantized/MNIST-Bottleneck-old/8/")
 
 elif EXP=="quantize-extra":
-    latex_MI("tanh", "quantized_layer_4_bits", "out/quantized_4bit/")
-    latex_MI("relu", "quantized_layer_4_bits", "out/quantized_4bit/")
-    latex_accuracy(["relu","tanh"],"out/quantized_4bit/", suffix="_4bit")
-    latex_MI("tanh", "quantized_layer_32_bits", "out/quantized_32bit/")
-    latex_MI("relu", "quantized_layer_32_bits", "out/quantized_32bit/")
-    latex_accuracy(["relu","tanh"],"out/quantized_32bit/", suffix="_32bit")
-
-# Prepare Tanh quantized
-#latex_MI("tanh_fixed_quantized", "quantized_fixed", "quantized_tanh/")
-#latex_MI("tanh_quantized", "quantized_neuron", "quantized_tanh/")
-
-
-# Accuracy
-#latex_accuracy(["relu","tanh"],"accuracy/")
-
-#latex_accuracy(["relu","tanh","relu_quantized","tanh_quantized"],"accuracy/")
-
-# ReLU comparison uniform vs adaptive binning
-#latex_MI("relu","binning_uniform_30","relu_binning_comparison/")
-#latex_MI("relu","binning_adaptive_30","relu_binning_comparison/")
-# ReLU quantized
-#latex_MI("relu_quantized","binning_quantized_30","relu_quantized/")
-
-# tanh comparison uniform vs adaptive binning
-#latex_MI("tanh","binning_uniform_30","tanh_binning_comparison/")
-#latex_MI("tanh","binning_adaptive_30","tanh_binning_comparison/")
-# tanh quantized
-#latex_MI("tanh_quantized","binning_quantized_30","tanh_quantized/")
-
-#for quant in ["4","32"]:
-#    for act in ["relu","tanh"]:
-#        latex_MI(act+"_"+quant+"bit", "quantized_layer_"+quant+"_bits", quant+"bit/"+act+"_")
-#
-#    latex_accuracy(["relu_"+quant+"bit","tanh_"+quant+"bit"], quant+"bit/")
+    # Plots for 4,32-bit quantized synthetic experiments: Tanh and ReLU, I-plane, accuracy
+    for b in ["4","32"]:
+        latex_MI("quantized_layer_"+b+"_bits", "SYN-Tanh/"+b+"/", "out/quantized/SYN-Tanh/"+b+"/")
+        latex_MI("quantized_layer_"+b+"_bits", "SYN-ReLU/"+b+"/", "out/quantized/SYN-ReLU/"+b+"/")
+        latex_accuracy("SYN-Tanh/"+b+"/", "out/quantized/SYN-Tanh/"+b+"/")
+        latex_accuracy("SYN-ReLU/"+b+"/", "out/quantized/SYN-ReLU/"+b+"/")
+    # Plots for 8-bit quantized MNIST-10 experiment: I-plane, accuracy
+    latex_MI("quantized_layer_8_bits", "MNIST-10/", "out/quantized/MNIST-10/8/")
+    latex_accuracy("MNIST-10/", "out/quantized/MNIST-10/8/")
 
